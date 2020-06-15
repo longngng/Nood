9d19b10b252e0c3b5465045b5e9e6593d58d0d65
Tra Quang Minh Thong <tqmthong@gmail.com>
2020-06-15 16:28:31 +0800

    update with button

--- React_frame/src/component/pages/KentRidge.js
+++ React_frame/src/component/pages/KentRidge.js
@@ -4,7 +4,7 @@ export default class KentRidge extends Component {
     render() {
         return (
             <div className = "halfKR">
-                
+                <h3>𝙺𝚎𝚗𝚝 𝚁𝚒𝚍𝚐𝚎 𝙲𝚊𝚖𝚙𝚞𝚜</h3>
             </div>
         )
     }
