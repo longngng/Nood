ce314bee9dbde4fa34af58f7a4d15b7748e2bd0f
Long Nguyen <ngoclong0x@gmail.com>
2020-06-19 22:25:31 +0800

    Add routers files

--- React_frame/backend/models/canteen.model.js
+++ React_frame/backend/models/canteen.model.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const canteenSchema = new Schema({
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: Date, required: true },
+  canteenname: { type: String, required: true },
+  description: { type: String},
+  //description: { type: String, required: true },
+  //duration: { type: Number, required: true },
+  //date: { type: Date, required: true },
 }, {
   timestamps: true,
 });
