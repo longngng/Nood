3c101d439a593167099be6bce0fa0de05009c59f
Long Nguyen <ngoclong0x@gmail.com>
2020-06-24 22:45:29 +0800

    Rearrange folder to be ready for deploying to Heroku

--- server.js
+++ server.js
@@ -25,14 +25,16 @@ const usersRouter = require('./routes/users');
 app.use('/canteens', canteensRouter);
 app.use('/users', usersRouter);
 
-//Serve static asset in production
-if(process.env.NODE_ENV === 'production') {
-  //Set static folder
-  app.use(express.static('build'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  })
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
