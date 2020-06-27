c331d7d93aa7507dd7f5239d81429a517e5f2df6
Tra Quang Minh Thong <tqmthong@gmail.com>
2020-06-28 02:14:57 +0800

    update data

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,1 +1,12 @@
-const express = require.apply()
+const express = require('express');
+const router = express.Router();
+
+//@route Get api/auth
+//@desc Test route
+//@acess Public
+
+router.get('/',(req,res) => {
+    res.send('Auth route');
+})
+
+module.exports = router;
