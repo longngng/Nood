03344ce98d28cecc0711ceae9b4e0b36750abe58
longnn <ngoclong0x@gmail.com>
2020-07-08 17:10:33 +0800

    Add login system with Alert

--- client/src/App.js
+++ client/src/App.js
@@ -1,110 +1,141 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Todos from './component/Todos';
-import Header_1 from './component/layout/header';
-import Header_2 from './component/layout/header2';
-import SideDrawer from './component/layout/SideDrawer';
-import Backdrop from './component/layout/Backdrop';
-import Footer from './component/layout/footer';
-import Addtodo from './component/addTodo';
-import TestInfo from './component/Res_template/Info/Info'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Todos from "./component/Todos";
+import Header_1 from "./component/layout/header";
+import Header_2 from "./component/layout/header2";
+import SideDrawer from "./component/layout/SideDrawer";
+import Backdrop from "./component/layout/Backdrop";
+import Footer from "./component/layout/footer";
+import Addtodo from "./component/addTodo";
+import TestInfo from "./component/Res_template/Info/Info";
 //import About from './component/layout/about';
-import AboutUs from './component/pages/Aboutus';
-import NUS_background from './component/layout/nus_background';
-import NUS_campuses from './component/layout/campuses';
-import NUS_campuses2 from './component/layout/campuses2';
-import NUS_campuses3 from './component/layout/campuses3';
+import AboutUs from "./component/pages/Aboutus";
+import NUS_background from "./component/layout/nus_background";
+import NUS_campuses from "./component/layout/campuses";
+import NUS_campuses2 from "./component/layout/campuses2";
+import NUS_campuses3 from "./component/layout/campuses3";
 // import {Container, Row, Col} from 'react-bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
+import axios from "axios";
 // import {v4 as uuid} from "uuid";
-import './App.css';
+import "./App.css";
 // import { ReactComponent } from '*.svg';
-import Homepage from './component/pages/Homepage';
-import KentRidge from './component/pages/KentRidge';
-import KentRidge2 from './component/pages/KentRidge2';
-import BukitTimah from './component/pages/BukitTimah';
-import UTown from './component/pages/UTown';
+import Homepage from "./component/pages/Homepage";
+import KentRidge from "./component/pages/KentRidge";
+import KentRidge2 from "./component/pages/KentRidge2";
+import BukitTimah from "./component/pages/BukitTimah";
+import UTown from "./component/pages/UTown";
 // import { ReactComponent } from '*.svg';
-import Search from './component/pages/search';
-import A_and_S from './component/Res_template/template_res';
-class App extends Component {
-  
-  render() {
-    
-    return (
-      <Router>
-        <div className = "App" style = {{height: '100%'}}>
+import Search from "./component/pages/search";
+import A_and_S from "./component/Res_template/template_res";
+import Login from "./component/auth/Login";
+import Register from "./component/auth/Register";
 
-              <Route path="/" exact component={Homepage} />
-              <Route path="/KentRidge2" component={KentRidge2} />
-              <Route path="/Info" component={TestInfo} />
-              
-              <Route exact path = "/BukitTimah" render = { props => (
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Alert from "./component/layout/Alert";
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <Alert />
+          <div className="App" style={{ height: "100%" }}>
+            <Route path="/" exact component={Homepage} />
+            <Route path="/KentRidge2" component={KentRidge2} />
+            <Route path="/Info" component={TestInfo} />
+            <Route path="/Login" component={Login} />
+            <Route path="/Register" component={Register} />
+
+            <Route
+              exact
+              path="/BukitTimah"
+              render={(props) => (
                 <React.Fragment>
-                  <Header_2/>
-                  <Addtodo/>
-                  <BukitTimah/>
+                  <Header_2 />
+                  <Addtodo />
+                  <BukitTimah />
                 </React.Fragment>
-              )} />
-              <Route exact path = "/UTown" render = { props => (
+              )}
+            />
+            <Route
+              exact
+              path="/UTown"
+              render={(props) => (
                 <React.Fragment>
-                  <Header_2/>
-                  <Addtodo/>
-                  <UTown/>
+                  <Header_2 />
+                  <Addtodo />
+                  <UTown />
                 </React.Fragment>
-              )} />
-              <Route exact path = "/Aboutus" render = { props => (
+              )}
+            />
+            <Route
+              exact
+              path="/Aboutus"
+              render={(props) => (
                 <React.Fragment>
-                  <Header_2/>
-                  <AboutUs/>
+                  <Header_2 />
+                  <AboutUs />
                 </React.Fragment>
-              )} />
-              <Route exact path = "/Search_result" render = { props => (
+              )}
+            />
+            <Route
+              exact
+              path="/Search_result"
+              render={(props) => (
                 <React.Fragment>
-                  <Search/>
+                  <Search />
                 </React.Fragment>
-              )} />
-              <Route exact path = "/A_and_S" render = { props => (
+              )}
+            />
+            <Route
+              exact
+              path="/A_and_S"
+              render={(props) => (
                 <React.Fragment>
-                  <Header_2/>
+                  <Header_2 />
                   {/* <Addtodo/> */}
-                  <A_and_S/>
-                  <Footer/>
+                  <A_and_S />
+                  <Footer />
                 </React.Fragment>
-              )} />
-        </div>  
-        <script src = "./component/layout/control_nav"></script>
-      </Router>
+              )}
+            />
+          </div>
+          <script src="./component/layout/control_nav"></script>
+        </Router>
+      </Provider>
     );
   }
 }
 export default App;
-  //state = {
-  //   todos: [
+//state = {
+//   todos: [
 
-  //   ],
-  //   sideDrawerOpen: false,
+//   ],
+//   sideDrawerOpen: false,
 
-  // }
+// }
 
-  // drawerToggleClickHandler = () => {
-  //   this.setState ((prevState) => {
-  //     return {sideDrawerOpen: !prevState.sideDrawerOpen};
-  //   });
-  // }
-  // backdropClickHandler = () => {
-  //   this.setState({sideDrawerOpen: false});
-  // }
-  
-  // let sidedrawer;
-  // let backdrop;
-  // if (this.state.sideDrawerOpen) {
-  //   sidedrawer = <SideDrawer/>;
-  //   backdrop = <Backdrop click = {this.backdropClickHandler}/>;
-  // }
-{/* <Route
+// drawerToggleClickHandler = () => {
+//   this.setState ((prevState) => {
+//     return {sideDrawerOpen: !prevState.sideDrawerOpen};
+//   });
+// }
+// backdropClickHandler = () => {
+//   this.setState({sideDrawerOpen: false});
+// }
+
+// let sidedrawer;
+// let backdrop;
+// if (this.state.sideDrawerOpen) {
+//   sidedrawer = <SideDrawer/>;
+//   backdrop = <Backdrop click = {this.backdropClickHandler}/>;
+// }
+{
+  /* <Route
                 exact
                 path="/Homepage2"
                 render={props => (
@@ -126,4 +157,5 @@ export default App;
                   <Addtodo/>
                   <KentRidge/>
                 </React.Fragment>
-              )} /> */}
+              )} /> */
+}
--- client/src/component/layout/header2.js
+++ client/src/component/layout/header2.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import '../../App'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../App";
 function header_2() {
-    return (
-        <nav style = {headerStyle}>
-            <Link onClick = {scrollToTop} style = {linkStylehead} to = "/">𝓝𝓤𝓢 𝓕𝓸𝓸𝓭</Link>
-            <div className = "nav-links">
-            <Link onClick = {scrollToTop} style = {linkStyle}to="/" className = "Linkbtn" activeStyle = {{color: "rgb(221, 160, 95)"}}>Home</Link>
-            <Link onClick = {scrollToTop} style = {linkStyle}to="/Register" className = "Linkbtn" activeStyle = {{color: "rgb(221, 160, 95)"}}>Register</Link>
-            {/* <Link style = {linkStyle} to = "/" className = "Homebtn"></Link> */}
-            </div>
-        </nav>
-    )
+  return (
+    <nav style={headerStyle}>
+      <Link onClick={scrollToTop} style={linkStylehead} to="/">
+        𝓝𝓤𝓢 𝓕𝓸𝓸𝓭
+      </Link>
+      <div className="nav-links">
+        <Link
+          onClick={scrollToTop}
+          style={linkStyle}
+          to="/"
+          className="Linkbtn"
+          activeStyle={{ color: "rgb(221, 160, 95)" }}
+        >
+          Home
+        </Link>
+        <Link
+          onClick={scrollToTop}
+          style={linkStyle}
+          to="/Register"
+          className="Linkbtn"
+          activeStyle={{ color: "rgb(221, 160, 95)" }}
+        >
+          Register
+        </Link>
+        <Link
+          onClick={scrollToTop}
+          style={linkStyle}
+          to="/Login"
+          className="Linkbtn"
+          activeStyle={{ color: "rgb(221, 160, 95)" }}
+        >
+          Login
+        </Link>
+        {/* <Link style = {linkStyle} to = "/" className = "Homebtn"></Link> */}
+      </div>
+    </nav>
+  );
 }
 // const headerStyle = {
 //     color: '#fff',
@@ -24,7 +51,7 @@ function header_2() {
 // const linkStyle = {
 //     color: '#fff',
 //     textAlign: 'center',
-//     float: 'right'   
+//     float: 'right'
 // }
 // const linkStylehead = {
 //     color: '#fff',
@@ -32,26 +59,26 @@ function header_2() {
 //     padding: '10px'
 // }
 const headerStyle = {
-    color: '#fff',
-    textAlign: 'center',
-    position: 'fixed',
-    background: '#EF7C00',
-}
+  color: "#fff",
+  textAlign: "center",
+  position: "fixed",
+  background: "#EF7C00",
+};
 
 const linkStyle = {
-    color: '#fff',
-    textAlign: 'center',
-    display: 'flex',
-    float: 'right',
-}
+  color: "#fff",
+  textAlign: "center",
+  display: "flex",
+  float: "right",
+};
 const linkStylehead = {
-    color: '#fff',
-    position: 'absolute',
-    top: '7px',
-    left: '100px',
-    display: 'flex'
-}
+  color: "#fff",
+  position: "absolute",
+  top: "7px",
+  left: "100px",
+  display: "flex",
+};
 const scrollToTop = () => {
-    window.scrollTo(0, 0)
-}
+  window.scrollTo(0, 0);
+};
 export default header_2;
