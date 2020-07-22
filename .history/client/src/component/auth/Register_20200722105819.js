9cbad4dccd08008892f38756f1ddcae6bc0af40d
Tra Quang Minh Thong <tqmthong@gmail.com>
2020-07-22 16:14:55 +0800

    Conflicts

---
+++ .history/client/src/component/auth/Register_20200722105819.js
@@ -0,0 +1,219 @@
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import {  Link,Redirect } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+import PropTypes from "prop-types";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import './Register.css'
+const Register = ({ setAlert, register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert("Passwords do not match", "danger");
+    } else {
+      //console.log(formData);
+      register({ name, email, password });
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to="/" />;
+  }
+  return (
+    // <Fragment>
+    //   <h1 className="large text-primary">Sign Up</h1>
+    //   <p className="lead">
+    //     <i className="fas fa-user" /> Create Your Account
+    //   </p>
+    //   <form className="form" onSubmit={onSubmit}>
+    //     <div className="form-group">
+    //       <input
+    //         type="text"
+    //         placeholder="Name"
+    //         name="name"
+    //         value={name}
+    //         onChange={onChange}
+    //       />
+    //     </div>
+    //     <div className="form-group">
+    //       <input
+    //         type="email"
+    //         placeholder="Email Address"
+    //         name="email"
+    //         value={email}
+    //         onChange={onChange}
+    //       />
+    //       <small className="form-text">
+    //         This site uses Gravatar so if you want a profile image, use a
+    //         Gravatar email
+    //       </small>
+    //     </div>
+    //     <div className="form-group">
+    //       <input
+    //         type="password"
+    //         placeholder="Password"
+    //         name="password"
+    //         value={password}
+    //         onChange={onChange}
+    //       />
+    //     </div>
+    //     <div className="form-group">
+    //       <input
+    //         type="password"
+    //         placeholder="Confirm Password"
+    //         name="password2"
+    //         value={password2}
+    //         onChange={onChange}
+    //       />
+    //     </div>
+    //     <input type="submit" className="btn btn-primary" value="Register" />
+    //   </form>
+    //   <p className="my-1">
+    //     Already have an account? <Link to="/login">Sign In</Link>
+    //   </p>
+    //   <p>Or back to <Link to = "/">Main page</Link></p>
+    // </Fragment>
+    <Grid container component="main" className="root">
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className="image" />
+      <Grid item xs={12} sm={8} md={5} component = {Paper} elevation={6} square>
+        <div className="paper">
+          <Avatar className="avatar">
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className="form" noValidate onSubmit = {onSubmit}>
+          <TextField
+              // variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value = {email}
+              onChange = {onChange}
+            />
+            <small className="form-text">
+               This site uses Gravatar so if you want a profile image, use a
+               Gravatar email
+            </small>
+            <TextField
+              // variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+              value = {name}
+              onChange = {onChange}
+            />
+            <TextField
+              // variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value = {password}
+              // autoComplete="current-password"
+              onChange = {onChange}
+              minLength = "6"
+            />
+            <TextField
+              // variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password2"
+              label="Confirm password"
+              type="password"
+              id="password2"
+              value = {password2}
+              // autoComplete="current-password"
+              onChange = {onChange}
+              minLength = "6"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className="submit"
+            >
+              Sign Up
+            </Button>
+            {/* <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Already have an account? Sign In"}
+                </Link>
+              </Grid>
+            </Grid> */}
+            <p className="my-1">
+              Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+            <p>Or back to <Link to = "/">Main page</Link></p>
+            {/* <Box mt={5}>
+              <Copyright />
+            </Box> */}
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+export default connect(mapStateToProps, { setAlert, register })(Register);
