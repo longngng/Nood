24578ebc1a372b4c1b2ace0f7f0095df7de3b714
Tra Quang Minh Thong <tqmthong@gmail.com>
2020-07-24 10:21:10 +0800

    add comment's front end

---
+++ .history/client/src/component/Res_template/template2_res_20200723150704.js
@@ -0,0 +1,120 @@
+import React, { Fragment, useEffect, useState } from "react";
+import "./Res.css";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Star from "../StarRating/StarRating";
+import Dish_1 from "./List_of_dishes/Dishes";
+import Info from "./Info/Info";
+import Header from "../layout/header";
+import Navbar from "../layout/Navbar3";
+import SideDrawer from "../layout/SideDrawer3_1";
+import Backdrop from "../layout/Backdrop";
+import Comment from "../Res_template/PostForm/PostForm";
+import PostItem from "./PostItem";
+import PostForm from "./PostForm";
+import { getPosts } from "../../actions/post";
+import { login } from "../../actions/auth";
+import { Redirect } from "react-router-dom";
+
+const Posts = ({ getPosts, post: { posts }, isAuthenticated }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  // const [sideDrawerOpen, drawerToggleClickHandler] = useState(false);
+  const [sideDrawerOpen, backdropClickHandler] = useState(false);
+    let sidedrawer;
+    let backdrop;
+    if (sideDrawerOpen) {
+      sidedrawer = <SideDrawer />;
+      backdrop = <Backdrop click={() => backdropClickHandler(!sideDrawerOpen)} />;
+    }
+  //   if (isAuthenticated) {
+  //     return <Redirect to="/A_and_S_std" />;
+  //   }
+  if (isAuthenticated) {
+    return (
+      <Fragment>
+        <Header drawerClickHandler = {() => backdropClickHandler(!sideDrawerOpen)} />
+        <Navbar />
+        {sidedrawer}
+        {backdrop}
+        <div className="background">
+          <br />
+          <br />
+          <div className="background_container">
+            <div className="res_name">
+              <h6>𝗔𝗿𝗶𝘀𝗲 𝗮𝗻𝗱 𝗦𝗵𝗶𝗻𝗲</h6>
+            </div>
+            <div className="top_favorit">
+              <h6>𝗦𝗶𝗴𝗻𝗮𝘁𝘂𝗿𝗲 𝗱𝗶𝘀𝗵𝗲𝘀</h6>
+            </div>
+            <div className="res_image"></div>
+            <div className="dish_image">
+              <Dish_1 />
+            </div>
+            <div className="intro">
+              <Info />
+            </div>
+            <div className="rating">
+              <Star />
+            </div>
+          </div>
+        </div>
+
+        <PostForm />
+        <div className="posts">
+          {posts.map((post) => (
+            <PostItem key={post._id} post={post} />
+          ))}
+        </div>
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+        <Header drawerClickHandler = {() => backdropClickHandler(!sideDrawerOpen)}/>
+        <Navbar />
+        {sidedrawer}
+        {backdrop}
+        <div className="background">
+          <br />
+          <br />
+          <div className="background_container">
+            <div className="res_name">
+              <h6>𝗔𝗿𝗶𝘀𝗲 𝗮𝗻𝗱 𝗦𝗵𝗶𝗻𝗲</h6>
+            </div>
+            <div className="top_favorit">
+              <h6>𝗦𝗶𝗴𝗻𝗮𝘁𝘂𝗿𝗲 𝗱𝗶𝘀𝗵𝗲𝘀</h6>
+            </div>
+            <div className="res_image"></div>
+            <div className="dish_image">
+              <Dish_1 />
+            </div>
+            <div className="intro">
+              <Info />
+            </div>
+            <div className="rating">
+              <Star />
+            </div>
+            <div className = "comment">
+              <h2>Login to comment</h2>
+            </div>
+          </div>
+        </div>
+        </Fragment>
+    );
+  }
+};
+
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
---
+++ .history/client/src/component/layout/Navbar_20200723230615.js
@@ -0,0 +1,143 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
+import DrawToggleButton from "./drawToggleButton";
+import { Link as LinkComp} from "react-scroll";
+
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+      <div class = "nav-links">
+        <LinkComp
+          activeClass="active"
+          to="nus"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={1000}
+          className="Linkbtn"
+          style={linkStyle}
+        >
+          <h5>Home</h5>
+        </LinkComp>
+        <LinkComp
+          activeClass="active"
+          to="campuses"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={1000}
+          className="Linkbtn"
+          style={linkStyle}
+        >
+         <h5>Explore</h5> 
+        </LinkComp>
+
+        <Link style={linkStyle} to="/Aboutus" className="Linkbtn">
+          <h5>About</h5>
+        </Link>
+        <Link style={linkStyle} to="/dashboard" className="Linkbtn">
+          <h5>Dashboard</h5>
+        </Link>
+
+        <a onClick={logout} href="#!" style ={linkStyle} className = "Linkbtn">
+          <h5>Logout</h5>
+        </a>
+ 
+    </div>
+  );
+
+  const guestLinks = (
+    <div class="nav-links">
+        <LinkComp
+          activeClass="active"
+          to="nus"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={1000}
+          className="Linkbtn"
+          style={linkStyle}
+        >
+          <p>Home</p>
+        </LinkComp>
+        <LinkComp
+          activeClass="active"
+          to="campuses"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={1000}
+          className="Linkbtn"
+          style={linkStyle}
+        >
+          <h4>Explore</h4>
+        </LinkComp>
+        <Link style={linkStyle} to="/Aboutus" className="Linkbtn" onClick = {scrollToTop}>
+          <h4>About</h4>
+        </Link>
+        <Link style={linkStyle} to="/Register" className="Linkbtn" onClick = {scrollToTop}>
+          <h4>Register</h4>
+        </Link>
+        <Link style={linkStyle} to="/Login" className="Linkbtn" onClick = {scrollToTop}>
+          <h4>Login</h4>
+        </Link>
+      </div>
+  );
+
+  return (
+    <nav style = {headerStyle}>
+      <Link
+        activeClass="active"
+        to="/"
+        spy={true}
+        smooth={true}
+        offset={0}
+        duration={500}
+        className="link"
+        style={linkStylehead}
+        onClick = {scrollToTop}
+      > 
+        𝓝𝓤𝓢 𝓕𝓸𝓸𝓭
+      </Link>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+const headerStyle = {
+  color: "#fff",
+  textAlign: "center",
+  position: "fixed",
+  backgroundColor: "#EF7C00",
+};
+
+const linkStyle = {
+  color: "#fff",
+  textAlign: "center",
+  display: "flex",
+  float: "right",
+};
+const linkStylehead = {
+  color: "#fff",
+  position: "absolute",
+  top: "7px",
+  left: "100px",
+  display: "flex",
+};
+const scrollToTop = () => {
+  window.scrollTo(0, 0);
+};
+export default connect(mapStateToProps, { logout })(Navbar);
