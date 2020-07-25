ce8a1f55a505779425c3dcb33d30dace649ebf1a
Tra Quang Minh Thong <tqmthong@gmail.com>
2020-07-25 21:53:41 +0800

    Fix dashboard apperance

--- client/src/component/dashboard/Dashboard.js
+++ client/src/component/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
@@ -6,7 +6,10 @@ import DashboardActions from "./DashboardActions";
 import Experience from "./Experience";
 import Education from "./Education";
 import { getCurrentProfile, deleteAccount } from "../../actions/profile";
-import Navbar from "../layout/Navbar";
+import Navbar from "../layout/Navbar2";
+import Header from "../layout/header";
+import SideDrawer from "../layout/SideDrawer2";
+import Backdrop from "../layout/Backdrop";
 
 const Dashboard = ({
   getCurrentProfile,
@@ -17,18 +20,31 @@ const Dashboard = ({
   useEffect(() => {
     getCurrentProfile();
   }, [getCurrentProfile]);
-
+  const [sideDrawerOpen, backdropClickHandler] = useState(false);
+  let sidedrawer;
+  let backdrop;
+  if (sideDrawerOpen) {
+    sidedrawer = <SideDrawer />;
+    backdrop = <Backdrop click={() => backdropClickHandler(!sideDrawerOpen)} />;
+  }
   return (
+    <div style = {{backgroundColor: 'aliceblue', minHeight: '100vh'}}>
     <Fragment>
+      <Header
+          drawerClickHandler={() => backdropClickHandler(!sideDrawerOpen)}
+        />
+      {sidedrawer}
+      {backdrop}
       <Navbar />
       <br />
       <br />
       <br />
-      <h1 className="large text-primary">Dashboard</h1>
+      <h1 className="large text-primary" style = {{color: 'rgb(87, 86, 42)'}}>Dashboard</h1>
       <p className="lead">
         <i className="fas fa-user" /> Welcome {user && user.name}
       </p>
     </Fragment>
+    </div>
   );
 };
 
